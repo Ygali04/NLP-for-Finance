deb3f8a9ec174fe33e87ddfb2bd08f6f330f0324
Yahvin Gali <115979938+Ygali04@users.noreply.github.com>
2022-11-05 14:53:13 -0700

    Create Loss.py

---
+++ Loss.py
@@ -0,0 +1,15 @@
+import Dataset
+import Preprocess
+import FinanceNetwork
+
+training_loss = history.history['loss']
+validation_loss = history.history["val_loss"]
+
+fig = plt.figure(figsize=(12,6))
+plt.title('Loss over Time')
+plt.xlabel('Epochs')
+plt.ylabel('Loss')
+plt.plot(training_loss, label="train")
+plt.plot(validation_loss, label="validation")
+plt.legend()
+plt.show()
